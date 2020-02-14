93e308915831fa1e44e8383bdfc9372b982e81d7
Numin <xnuminousx@gmail.com>
2020-02-13 19:28:19 -0600

    some changes/improvements - Added a message players receive when they get a kill. (configurable) - Reformatted the code used for healing players. - Changed the version used for the ProjectKorra API

--- src/main/java/me/numin/siphon/Config.java
+++ src/main/java/me/numin/siphon/Config.java
@@ -31,12 +31,14 @@ public class Config {
 
         config.addDefault("HealthRegenerationAmount.OnEntityKill", 2);
         config.addDefault("HealthRegenerationAmount.OnPlayerKill", 4);
+        config.addDefault("SendHealMessage", true);
 
         config.options().copyDefaults(true);
         plugin.saveConfig();
     }
 
     public static List<String> getEnabledWorlds() { return plugin.getConfig().getStringList("EnabledWorlds"); }
+    public static boolean canSendHealMessage() { return plugin.getConfig().getBoolean("SendHealMessage"); }
     public static double getEntityKillAmount() { return plugin.getConfig().getDouble("HealthRegenerationAmount.OnEntityKill"); }
     public static double getPlayerKillAmount() { return plugin.getConfig().getDouble("HealthRegenerationAmount.OnPlayerKill"); }
 }
--- src/main/java/me/numin/siphon/SiphonListener.java
+++ src/main/java/me/numin/siphon/SiphonListener.java
@@ -1,6 +1,7 @@
 package me.numin.siphon;
 
 import com.projectkorra.projectkorra.event.EntityBendingDeathEvent;
+import org.bukkit.ChatColor;
 import org.bukkit.Particle;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -13,19 +14,21 @@ public class SiphonListener implements Listener {
         Entity entity = event.getEntity();
         Player killer = event.getAttacker();
         String world = killer.getWorld().getName();
-        double killerHealth = killer.getHealth();
 
         if (!Config.getEnabledWorlds().contains(world))
             return;
 
-        if (killerHealth >= 20)
+        if (killer.isDead() || killer.getHealth() >= 20)
             return;
 
+        healKiller(killer, entity);
+    }
+
+    private void healKiller(Player killer, Entity entity) {
+        double healAmount = toHealAmount(killer.getHealth(), entity instanceof Player ? Config.getPlayerKillAmount() : Config.getEntityKillAmount());
+        killer.setHealth(killer.getHealth() + healAmount);
         killer.getWorld().spawnParticle(Particle.HEART, killer.getLocation().add(0, 1.7, 0), 1, 0, 0, 0);
-        if (entity instanceof Player)
-            killer.setHealth(killerHealth + toHealAmount(killerHealth, Config.getPlayerKillAmount()));
-        else
-            killer.setHealth(killerHealth + toHealAmount(killerHealth, Config.getEntityKillAmount()));
+        if (Config.canSendHealMessage()) killer.sendMessage(ChatColor.GREEN + "" + ChatColor.ITALIC + "+" + healAmount / 2);
     }
 
     private double toHealAmount(double playerHealth, double targetHeal) {
